808b445746e5e24919fb1a2596b381f5a8d1e2a5
Ravindu Rashmika <ravindu123lasky@gmail.com>
2020-10-04 16:58:15 +0530

    Final Commit

---
+++ app/src/main/java/com/example/goodreads/Prevalent/Prevalent.java
@@ -0,0 +1,12 @@
+package com.example.goodreads.Prevalent;
+
+import com.example.goodreads.Model.Users;
+
+public class Prevalent {
+
+    private static Users CurrentOnlineUser;
+
+    //remember me function
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
